394948c6dd9f1fad2b80555498c7c4769444c7eb
hjz <3200105450@zju.edu.cn>
2023-05-26 18:49:41 +0800

    Struct (almost) done

---
+++ res/test.sol
@@ -0,0 +1,22 @@
+// function a() returns (bool) {
+//     return false;    
+// }
+// function aa(int aa) returns(bool){
+//     return true;
+// }
+// function main() returns () {
+    // int c[5];
+    // c[0]=1;
+    // c[1]=2;
+    int test = 3;
+    test = 2;
+    test += 1;
+    test = 3.5;
+    // 1<<2.0;
+    // test += 1;
+    // test = test + 1;
+    // test++;
+    // test = c[1];
+    // a();
+    // aa(1);
+// }
